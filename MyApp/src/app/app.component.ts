1472b36710f4422251baece207ae28a2e17e6574
Sadat Ali <sa9621952@gmail.com>
2026-04-07 01:57:15 +0530

    solve problem related to mCart application dependencies

--- MyApp/src/app/app.component.ts
+++ MyApp/src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MyApp';
+
+  export class AppComponent {
+  courses: any[] = [
+    { id: 1, name: 'TypeScript' },
+    { id: 2, name: 'Angular' },
+    { id: 3, name: 'Node JS' },
+    { id: 1, name: 'TypeScript' }
+  ];
 }
